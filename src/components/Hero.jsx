9c9ee11d726792ef4fb366e021cedcdf64fb77aa
Yusof Paciente <yusofpaciente29@gmail.com>
2023-05-03 05:34:51 +0800

    resume update

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -81,7 +81,7 @@ const Hero = () => {
           </a>
           <a
             className="transition ease-in duration-300 hover:bg-sky-400 flex items-center justify-center gap-2 py-2 px-4 bg-lightsecondary rounded-full text-base dark:bg-bglight text-bglight dark:text-bgdark dark:hover:bg-sky-400"
-            href="https://drive.google.com/file/d/1tQc9FSeUcg1jYttO4gafXYQf5Jt6RjHf/view?usp=share_link"
+            href="https://drive.google.com/file/d/1xWdhwsbL6-WXtxEcG_N5oiR9dGO6ysZB/view?usp=share_link"
             target="_blank"
             rel="noreferrer noopener"
             title="Download Resume - Yusof Paciente"
