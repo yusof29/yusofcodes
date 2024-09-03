2474bd00b6255c38b2d746e46cd5dd6707dbc5dd
Yusof <yusofpaciente29@gmail.com>
2024-09-04 06:26:37 +0800

    fix

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -2,7 +2,7 @@ import { BsCircle } from "react-icons/bs";
 
 const Skills = () => {
   const skills = [
-    "REACT",
+    "REACTS",
     "Javascript ES6",
     "Redux / Redux Toolkit",
     "HTML & CSS",
@@ -17,21 +17,21 @@ const Skills = () => {
   ];
 
   return (
-    <section name="skills" className="w-full min-h-screen flex items-center">
-      <div className="container mx-auto py-6 px-5 lg:px-10">
+    <section name="skills" className="flex min-h-screen w-full items-center">
+      <div className="container mx-auto px-5 py-6 lg:px-10">
         <div className="sm:w-3/5">
-          <h2 className="text-2xl lg:text-4xl font-bold text-sky-400 tracking-wider">
+          <h2 className="text-2xl font-bold tracking-wider text-sky-400 lg:text-4xl">
             My Skills
           </h2>
-          <p className="text-lightsecondary dark:text-darksecondary my-12">
+          <p className="my-12 text-lightsecondary dark:text-darksecondary">
             I develop web applications using these languages and frameworks. I
             am always excited by new technologies, therefore, I do not hesitate
             to take up coding task.
           </p>
         </div>
 
-        <div className="text-lightprimary dark:text-darkprimary font-semibold">
-          <ul className="grid sm:grid-cols-2 md:grid-cols-3 gap-5">
+        <div className="font-semibold text-lightprimary dark:text-darkprimary">
+          <ul className="grid gap-5 sm:grid-cols-2 md:grid-cols-3">
             {skills.map((skill, i) => (
               <li key={i} className="flex items-center py-2 tracking-wide">
                 <BsCircle className="mr-2 w-3 text-sky-300" />
